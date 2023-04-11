2550757d8747de28a3d3fa0cf17eedaf3ac316bc
KevinNizet <kevin.nizet@gmail.com>
2023-04-11 07:38:22 +0200

    ajout de style CSS sur les boutons

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "../style/Avatar.css";
+import "../style/style.css";
+
+function Avatar({ avatar }) {
+  const [donut, setDonut] = useState(0);
+  const handleDonut = () => setDonut(donut + 1);
+
+  return (
+    <figure className={"Avatar"}>
+      <img src={avatar.img} />
+      <div className="AvatarText">
+        <p>
+          {" "}
+          {avatar.firstName} {avatar.lastName}
+        </p>
+        <button onClick={handleDonut}> Donuts mangés : {donut} 🍩 </button>
+      </div>
+    </figure>
+  );
+}
+
+export default Avatar;
